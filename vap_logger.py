17d1b974fc59a932e0a3f7a44ff93060d4ea03a0
ksterne <kevintyler@vt.edu>
2018-01-05 14:09:42 -0500

    Adding in some additional inputs we'll need for logging

--- vap_logger.py
+++ vap_logger.py
@@ -1,20 +1,33 @@
+#! /usr/bin/python
 # Logging program for VA QSO party.
 
 # Reads in county, multiplier data from va_list.py
 
 
+import datetime as dt
+
 from va_list import *
 
 
+#print counties
 print counties["abbrev"]
 print counties["name"]
+
+# Need to add initial setup
+timenow = dt.datetime.utcnow()
+print timenow
+# Ask for call sign
+mycall = raw_input('Please enter your callsign: ')
+# Ask for location
+myqth = raw_input('Please entery your location: ')
+
 
 try:
     while (1):
 
-        entry = raw_input('Enter new QSO here: ')
+        entry = raw_input('Please enter new QSO: ')
 
-#        print entry
+        print entry
         entry.strip('\n')
 
         if entry in counties["abbrev"] or entry in counties["name"]:
