7419106db852925c511934c7a08276665d17e3da
ksterne <kevintyler@vt.edu>
2018-01-05 13:53:26 -0500

    initial commit of list of multipliers file, starting to build out on dicts

---
+++ va_list.py
@@ -0,0 +1,25 @@
+
+# List of counties/cities as well states and other multipliers for VA QSO Party
+
+counties = {}
+counties["abbrev"] = []
+counties["abbrev"] =      ['ACC', 'ALB', 'ALX', 'ALL', 'AME']
+counties["abbrev"].extend(['AMH', 'APP', 'ARL', 'AUG', 'BAT'])
+counties["abbrev"].extend(['BED', 'BLA', 'BOT', 'BRX', 'BRU'])
+counties["abbrev"].extend(['BCH', 'BHM', 'BVX', 'CAM', 'CLN'])
+counties["abbrev"].extend(['CRL', 'CCY', 'CHA', 'CHX', 'CPX'])
+counties["abbrev"].extend(['CHE', 'CLA', 'COX', 'CVX', 'CRA'])
+counties["abbrev"].extend(['CUL', 'CUM', 'DAX', 'DIC', 'DIN'])
+counties["abbrev"].extend(['EMX', 'ESS', 'FFX', 'FXX', 'FCX'])
+counties["abbrev"].extend(['FAU', 'FLO', 'FLU', 'FRA', 'FRX'])
+counties["abbrev"].extend(['FRE', 'FBX', 'GAX', 'GIL', 'GLO'])
+counties["abbrev"].extend(['GOO', 'GRA', 'GRN', 'GVL', 'HAL'])
+counties["abbrev"].extend(['HAX', 'HAN', 'HCO', 'HBX', 'HRY'])
+counties["abbrev"].extend(['HIG', 'HOX', 'IOW', 'JAM', 'KQN'])
+
+
+
+counties["name"] = ['Accomack', 'Albemarle', 'Alexandria', 'Alleghany', 'Amelia']
+
+
+#states = ['OH', 'TN', 'NC']
