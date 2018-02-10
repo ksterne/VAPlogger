77cbbd9365eae0ae9cbbf40147acf2dfc61831dd
ksterne <kevintyler@vt.edu>
2018-02-10 14:58:40 -0500

    Changing states list around some

--- va_list.py
+++ va_list.py
@@ -80,8 +80,10 @@ counties["name"] = ['Accomack', 'Albemarle', 'Alexandria',
                     'Winchester', 'Wise', 'Wythe', 'York']
 
 
-states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+states['abbrev'] = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC",
+                    "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA",
+                    "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN",
+                    "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM",
+                    "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI",
+                    "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA",
+                    "WV", "WI", "WY"]
